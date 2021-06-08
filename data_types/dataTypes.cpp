b9cec3316a32a105ff203c5b64098c0fc25ca0f8
pereiradaniel <danielpaulpereira@protonmail.com>
2021-06-08 19:57:16 -0400

    Add data_types

---
+++ data_types/dataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int yearOfBirth=1995;
+    char gender = 'm';
+    bool isOlderThan18=true;
+    float averageGrade = 4.5;
+    double balance = 45678945856;
+
+    cout << "Size of int is " <<sizeof(int) << " bytes\n";
+    cout << "Int min value is " << INT_MIN << endl;
+    cout << "Int max value is " << INT_MAX << endl;
+    cout << "Size of unsigned int is " << sizeof(unsigned int) << " bytes\n";
+    cout << "UInt max value is "<< UINT_MAX << endl;
+    system("pause>0");
+}
