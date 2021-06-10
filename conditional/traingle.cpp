b9cf84103a15642c10c47cb7bef47322feac1b3b
pereiradaniel <danielpaulpereira@protonmail.com>
2021-06-10 16:58:00 -0400

    triangle.cpp

---
+++ conditional/traingle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, c;
+    cout << "a, b, c: ";
+    cin >> a >> b >> c;
+
+    if (a==b && b==c)
+    {
+        cout << "Equilateral";
+    }
+    else if (a!=b && a!=c && b!=c)
+    {
+        cout << "Scalene";
+    }
+    else
+    {
+        cout << "Isosceles";
+    }
+
+    cout << endl << "Program complete";
+
+    system("pause>0");
+}
