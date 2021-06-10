29c762922cd26c05f0d0e0e472d6b62e8fb6355b
pereiradaniel <danielpaulpereira@protonmail.com>
2021-06-10 16:30:18 -0400

    Add ascii table

---
+++ ascii_table/asciiprogram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    // Get numeric value from character
+    cout << (int)'a' << endl; // casting operator
+    cout << int('a') << endl;
+    cout << int('A') << endl;
+
+    // Get character value from number
+    cout << char(65) << endl;
+
+    system("pause>0");
+}
