b7d2a32ac10211f6aaea8dbd0fc3344c92905c4c
pereiradaniel <danielpaulpereira@protonmail.com>
2021-06-22 10:02:37 -0400

    Add variadic templates example.

---
+++ w3_templates/variadic_templates/variadic.cpp
@@ -0,0 +1,16 @@
+// Variadic Templates
+// variadic.cpp
+#include <iostream>
+template <typename T>
+void print(const T& t) {
+    std::cout << t << std::endl;
+}
+template <typename T, typename... etc>
+void print(const T& t,const etc&... pp) { 
+    std::cout << t << " | ";
+    print(pp...);
+}
+int main() {
+    print(100);
+    print("abcd", 100, 34.56);
+}
