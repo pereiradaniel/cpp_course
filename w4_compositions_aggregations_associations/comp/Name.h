ec60eb4efd3816901fa11b39074e28e3e142f5f3
pereiradaniel <danielpaulpereira@protonmail.com>
2021-06-23 13:37:08 -0400

    Add w4

---
+++ w4_compositions_aggregations_associations/comp/Name.h
@@ -0,0 +1,15 @@
+#ifndef NAME_H
+#define NAME_H
+// Composition - Name
+// Name.h
+class Name {
+    char* name { nullptr };
+public:
+    Name(const char*);
+    Name(const Name&);
+    Name& operator=(const Name&); 
+    ~Name();
+    const char* get() const;
+    void set(const char*);
+};
+#endif
