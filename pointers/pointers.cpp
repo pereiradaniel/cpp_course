21dbb64fde26c55df2860d3126faf33e57fac36b
pereiradaniel <danielpaulpereira@protonmail.com>
2021-06-23 16:19:35 -0400

    Update pointers

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -6,6 +6,24 @@ int main()
     int n = 5;
     cout << &n << endl;
 
+    int* ptr = &n; // pointer holds the address of n variable
+    cout << ptr << endl;
+
+    // to access the value of ptr, it must be dereferenced
+    cout << *ptr << endl;
+
+    // to change the value of the address pointed to by ptr
+    *ptr = 14;
+    cout << *ptr << endl;
+    cout << n << endl;
+
+    // Pointers must be of the same type!:
+    //      int, int* ptr
+    int v;
+    int* ptr2 = &v;
+    *ptr2 = 7;
+    cout << "v=" << *ptr2 << endl;
+
     system("pause>0");
     return 0;
 }
