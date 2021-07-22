e2f7ce34fa175270a2f1c982dc06ea648424997c
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-22 15:17:23 -0400

    Checked and running.

--- digit_counter/digit_counter.cpp
+++ digit_counter/digit_counter.cpp
@@ -7,7 +7,15 @@ int main()
     int number;
     cout<<"Number: ";
     cin>>number;
-    
+
+    if (number==0)
+        cout<<"You have entered 0.";
+    else {
+        int counter=0;
+        while(number>0)
+            number/=10, counter++;
+        cout<<"Number has "<<counter<<" digits."<<endl;
+    }
 
     system("pause>0");
 }
