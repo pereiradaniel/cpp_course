81323dae30f00e05dad535e2930a621043efd2a0
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-22 16:08:51 -0400

    refactor

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -5,9 +5,8 @@ int main()
     int number, result = 1;
     cout << "Number: ";
     cin >> number;
-    for (int i = 1; i <= number; i++) {        
+    for (int i = 1; i <= number; i++)
         result *= i;
-    }
     cout<<"Factorial = "<<result<<endl;
     system("pause>0");
 }
