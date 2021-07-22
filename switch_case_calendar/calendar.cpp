7a3a80bc1720676d7501646446f3a69bd398c77c
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-22 14:49:10 -0400

    Set up file

---
+++ switch_case_calendar/calendar.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    int year, month;
+    cout<<"Year, month: ";
+    cin>>year>>month;
+
+    
+
+    system("pause>0");
+}
