294baefbd4b3a1ea65fb44c507bf865a56578b76
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-22 14:59:06 -0400

    finish calendar

--- switch_case_calendar/calendar.cpp
+++ switch_case_calendar/calendar.cpp
@@ -10,10 +10,20 @@ int main()
 
     switch (month)
     {
-        case 2:
-        (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? cout<<"29 days in this month.":cout<<"28 days in this month.";
-
+        case 2: 
+        (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? cout<<"29 days in this month.":cout<<"28 days in this month."; break;
+        case 4:
+        case 6:
+        case 9:
+        case 11: cout<<"30 days in this month."; break;
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12: cout<<"31 days in this month."; break;
+        default: cout<<"Invalid!";
     }
-
     system("pause>0");
 }
