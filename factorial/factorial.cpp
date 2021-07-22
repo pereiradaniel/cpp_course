b293d62dd2cc55d201791af1afbb63d83dffc6e8
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-22 16:06:29 -0400

    Complete factorial

---
+++ factorial/factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number, result = 1;
+    cout << "Number: ";
+    cin >> number;
+    for (int i = 1; i <= number; i++) {        
+        result *= i;
+    }
+    cout<<"Factorial = "<<result<<endl;
+    system("pause>0");
+}
