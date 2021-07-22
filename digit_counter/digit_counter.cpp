532a769f393c86fca6ffaca83e42d16936736b0f
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-22 15:19:21 -0400

    Handle negative ints

--- digit_counter/digit_counter.cpp
+++ digit_counter/digit_counter.cpp
@@ -11,6 +11,8 @@ int main()
     if (number==0)
         cout<<"You have entered 0.";
     else {
+        if (number<0)
+            number*=-1;
         int counter=0;
         while(number>0)
             number/=10, counter++;
