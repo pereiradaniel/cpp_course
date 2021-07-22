dbe602a61a47410aa4acd72aec90a13f4925de18
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-22 15:39:19 -0400

    Completed digit_reverser

--- digit_reverser/digit_reverser.cpp
+++ digit_reverser/digit_reverser.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
+    int number, result=0;
+    cout << "Number: ";
+    cin >> number;
+    while(number!=0) {
+        result*=10;
+        int lastDigit=number%10;
+        result+=lastDigit;
+        number/=10;
+    }
+    cout<<"Reversed: " <<result<<endl;
     system("pause>0");
 }
