07985670ac8d6df1bbb67c0081e1db74220cd1f4
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-22 15:05:25 -0400

    Add folder and file

---
+++ digit_counter/digit_counter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Count the digits of a number
+    int number;
+    cout<<"Number: ";
+    cin>>number;
+    
+
+    system("pause>0");
+}
