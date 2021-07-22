daa2b44a039305d94aa6c461488579939d375a11
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-22 14:39:11 -0400

    Complete switch case calc

--- switch_case_calc/calc.cpp
+++ switch_case_calc/calc.cpp
@@ -4,6 +4,35 @@ using namespace std;
 int main()
 {
     float num1, num2;
-    
+    char operation;     // add, subtract, multiply, divide, modulate or invalid
+    cout << "Calculator: ";
+    cin>>num1>>operation>>num2; //get use input
+
+    switch (operation)
+    {
+        case '-':  cout<<num1<<operation<<num2<<"="<<num1-num2;
+        break;
+        case '+':  cout<<num1<<operation<<num2<<"="<<num1+num2;
+        break;
+        case '*':  cout<<num1<<operation<<num2<<"="<<num1*num2;
+        break;
+        case '/':  cout<<num1<<operation<<num2<<"="<<num1/num2;
+        break;
+        // case '%':  cout<<num1<<operation<<num2<<"="<<(int)num1%(int)num2;
+        // break;
+
+        case '%':
+        bool isNum1Int, isNum2Int;
+        isNum1Int=((int)num1==num1);
+        isNum2Int=((int)num2==num2);
+
+        if (isNum1Int&&isNum2Int)
+            cout<<num1<<operation<<num2<<"="<<(int)num1%(int)num2;
+        else
+            cout << "Not valid";
+        break;
+
+    }
+
     system("pause>0");
 }
