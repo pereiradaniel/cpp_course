0aae929bf54077a6071800230e60bf2139a0386d
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-28 14:37:11 -0400

    build template

--- Codebeauty/generic_functions_and_templates/generic.cpp
+++ Codebeauty/generic_functions_and_templates/generic.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void swap(int& a, int& b) {
-    int temp=a;
-    a=b;
-    b=temp;
-}
-
-void swap(char& a, char& b) {
-    char temp=a;
+template<typename T>
+void Swap(T& a, T& b) {
+    T temp=a;   // T = generic data type
     a=b;
     b=temp;
 }
@@ -18,12 +13,12 @@ int main()
     int a=5, b=7;
     char c='c', d='d';
     cout<<a<<" - "<<b<<endl;
-    swap(a, b);
+    Swap<int>(a, b); // can specify the type by invocation
     cout<<a<<" - "<<b<<endl;
 
 
     cout<<c<<" - "<<d<<endl;
-    swap(c, d);
+    Swap<char>(c, d);
     cout<<c<<" - "<<d<<endl;
     system("pause>0");
     return 0;
