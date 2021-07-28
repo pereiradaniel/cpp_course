2ffb0f0d21bb45d1b9c326a9d03175d5f5273fb7
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-28 14:52:29 -0400

    complete recursion

--- Codebeauty/recursion/recursion.cpp
+++ Codebeauty/recursion/recursion.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int recursive_sum(int m, int n) {
+    if(m==n)
+        return m;
+    return m + recursive_sum(m+1, n);
+}
+
 // Sum numbers between m-n
 int main()
 {
@@ -10,10 +16,12 @@ int main()
         cin>>m>>n;
     }
 
-    for (int i=m; i<=n; i++)
-        sum += i;
+    cout << "Sum = " << recursive_sum(m, n);
 
-    cout<<"Sum = "<<sum<<endl;
+    // for (int i=m; i<=n; i++)
+    //     sum += i;
+
+    // cout<<"Sum = "<<sum<<endl;
 
     system("pause>0");
 }
