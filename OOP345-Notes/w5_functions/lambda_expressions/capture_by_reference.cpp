44d91925de75f845af27b57a03077771b042ea81
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-29 16:09:24 -0400

    Add unadded files

---
+++ OOP345-Notes/w5_functions/lambda_expressions/capture_by_reference.cpp
@@ -0,0 +1,18 @@
+// Lambda Expressions
+// capture_by_reference.cpp
+
+#include <iostream>
+
+template<typename Func>
+int add(int i, Func func) { return func(i); }
+
+template<typename Func>
+int sub(int i, Func func) { return func(i); }
+
+int main() {
+    int k = 4;
+    std::cout << add(10, [&](int i){ return i + k++; }) << std::endl; 
+    std::cout << "k = " << k << std::endl; 
+    std::cout << sub(10, [&](int i){ return i - k++; }) << std::endl;
+    std::cout << "k = " << k << std::endl; 
+}
