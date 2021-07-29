88facac9d99bc64c2b1f5640a394bc293c980bdd
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-29 15:52:32 -0400

    add w5_functions

---
+++ OOP345-Notes/w5_functions/auto_return.cpp
@@ -0,0 +1,44 @@
+// Trailing Return - Enumeration within a class
+// auto_return.cpp
+
+#include <iostream>
+
+class Ticket {
+public:
+    enum class TicketType { Adult, Child };
+    void set(TicketType tt);
+    TicketType get() const;
+private:
+    TicketType ticketType;
+};
+
+std::ostream& operator<<(std::ostream& os,
+const Ticket::TicketType& tt) {
+    const char* label = nullptr;
+    switch(tt) {
+        case Ticket::TicketType::Adult:
+            label = "Adult Ticket";
+            break;
+        case Ticket::TicketType::Child:
+            label = "Child Ticket";
+            break;
+        default:
+            label = "No Ticket";
+    }
+    os << label;
+    return os;
+}
+
+void Ticket::set(TicketType tt) { ticketType = tt; }
+
+auto Ticket::get() const -> TicketType { return ticketType; } 
+
+int main () {
+    Ticket a, b;
+    a.set(Ticket::TicketType::Adult);
+    b.set(Ticket::TicketType::Child);
+    std::cout << a.get() << std::endl;
+    std::cout << b.get() << std::endl;
+}
+
+// https://ict.senecacollege.ca/~oop345/pages/content/funct.html
