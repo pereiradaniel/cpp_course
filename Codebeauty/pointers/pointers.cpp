e6f1381e6213e6b8b2b2db994a4cb10e2751e3e8
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-07-29 14:34:32 -0400

    modify pointers

--- Codebeauty/pointers/pointers.cpp
+++ Codebeauty/pointers/pointers.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
-
+// Pointer stores an address to a memory location.
 int main()
 {
-    int n = 5;
-    cout << &n << endl;
+    int n = 5;          // n is a variable
+    cout << &n << endl; // the address of n
 
+    // Create a pointer by selecting a type and using the *
     int* ptr = &n; // pointer holds the address of n variable
-    cout << ptr << endl;
+    cout << ptr << endl;    // Outputs address of n
 
     // to access the value of ptr, it must be dereferenced
-    cout << *ptr << endl;
+    cout << *ptr << endl;   // 5
 
     // to change the value of the address pointed to by ptr
     *ptr = 14;
-    cout << *ptr << endl;
-    cout << n << endl;
+    cout << *ptr << endl;   // 14
+    cout << n << endl;      // 14
 
     // Pointers must be of the same type!:
     //      int, int* ptr
