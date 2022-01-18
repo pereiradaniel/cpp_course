eacd66f268aa328d8332b1970d40d1711123eced
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-01-17 20:02:17 -0500

    Added initializer to w2 oop

---
+++ OOP345-Notes/w2_fund_types/initializer.cpp
@@ -0,0 +1,16 @@
+// Initialization Expressions
+// initializer.cpp
+#include <iostream>
+int main() {
+    int n0 = 7;             // C-style
+    int n1 = 7.2;           // C-style, narrowing - lossy
+    int n2 = {6};           // universal form braces-enclosed list
+    int n3 = {5};           // = is redundant
+
+    std::cout<<"n0 = "<<n0<<std::endl;
+    std::cout<<"n1 = "<<n1<<std::endl;
+    std::cout<<"n2 = "<<n2<<std::endl;
+    std::cout<<"n3 = "<<n3<<std::endl;
+
+    return(0);
+}
