8f105d165e2d7677d49463dbcb0266d3ff2178f3
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-01-19 16:33:21 -0500

    Update notes and fix program

--- OOP345-Notes/w1_compilation/compile_time_evaluations/constexpr.cpp
+++ OOP345-Notes/w1_compilation/compile_time_evaluations/constexpr.cpp
@@ -1,5 +1,8 @@
 // Compiler-Evaluated Expressions
 // constexpr.cpp
+
+// In this example, the factorial calculation expressed in the form of a recursive function (a function that calls itself) does not depend on the rest of the program and is identified as a constant expression.  An constant expression can only refer to variables that are also constant expressions:
+
 #include <iostream>
 constexpr int N = 8; // constant variable
 constexpr int factorial(int i) { // constant function
--- OOP345-Notes/w1_compilation/static_assertions/static_assert.cpp
+++ OOP345-Notes/w1_compilation/static_assertions/static_assert.cpp
@@ -2,15 +2,16 @@
 // static_assert.cpp
 
 // The main function checks the value of N and reports an error if the value is outside practical bounds.
+
 #include <assert.h>
 #include <iostream>
 constexpr int N = 0;
 constexpr int factorial(int i) {
     return i > 1 ? i * factorial(i-1) : 1;
 }
+
 int main() {
-    // static_assert(2+2==4, "2+2 isn't 4");
-    static_assert(N > 0, "N <=  0"); // This line creates a compiler error even though this is how it appears in the example.
+    static_assert(N > 0, "N <=  0"); // static_assert.cpp(13): error C2338: N <=  0
     static_assert(N < 20, "N >= 20");
     int n {N};
     std::cout << n << "! = " << factorial(n) << std::endl;
