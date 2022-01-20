20800a6102911f21fa991ef7b07f74ee3d43e365
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-01-19 20:22:35 -0500

    Update

--- OOP345-Notes/w2_classes_enum/copy_assign.cpp
+++ OOP345-Notes/w2_classes_enum/copy_assign.cpp
@@ -1,5 +1,10 @@
 // Copy-Construction, Copy-Assignment and Subscripting Operator
 // copy_assign.cpp
+
+// Copy Construction and Copy Assignment
+
+// Classes that include resources manage them through user-defined copy constructors, copy-assignment operators and destructuctors.  In the following example, member initialization syntax simplifies the coding of these special member functions.  The C++ language allows dynamic memory allocation of 0 (zero) elements, but requires its destruction to complement it. 
+
 #include <iostream>	
 class Array {
     int* a = nullptr;
@@ -20,26 +25,26 @@ public:
         }
         return *this;
     }
-    // destructor
-    ~Array() { delete [] a; }
-    int& operator[](unsigned i) {
-        return n > 0u && i < n ? a[i] : dummy; }
-    int operator[](unsigned i) const {
-        return n > 0u && i < n ? a[i] : dummy; }
+    ~Array() { delete [] a; } // destructor
+    int& operator[](unsigned i) { return n > 0u && i < n ? a[i] : dummy; }
+    int operator[](unsigned i) const { return n > 0u && i < n ? a[i] : dummy; }
     unsigned size() const { return n; }
 };
+
 int main() {
     const unsigned size = 5;
-    Array a(size), b;
+    Array a(size), b; // 
     for (unsigned i = 0u; i < a.size(); ++i)
         a[i] = 3 * i;
     for (unsigned i = 0u; i < a.size(); ++i)
-        std::cout << a[i] << ' ';
+        std::cout << a[i] << ' '; // 0 3 6 9 12
     std::cout << std::endl;
-    b = a;
+    b = a; // copy-assignment
     for (unsigned i = 0u; i < b.size(); ++i)
-        std::cout << a[i] << ' ';
+        std::cout << a[i] << ' '; // 0 3 6 9 12
     std::cout << std::endl;
 };
 
 // This example also shows how to code lvalue and rvalue versions of the subscripting operator ([]).
+
+// This example also shows how to code lvalue and rvalue versions of the subscripting operator ([]).  The lvalue version returns a reference to either an element of the array or to the dummy instance variable.  Both versions check that the index received is within bounds and if outside refer to this dummy variable.  This ensures that any change by the lvalue version of the subscripting operator does not corrupt memory outside the memory referred to by the object. 
--- OOP345-Notes/w2_classes_enum/initialize.cpp
+++ OOP345-Notes/w2_classes_enum/initialize.cpp
@@ -2,26 +2,28 @@
 // initialize.cpp
 #include <iostream>
 #include <string>
+
 int nItems = 0;
+
 class Item{
     int item = ++nItems;              // default initializer
     const std::string name {"empty"}; // default initializer 
 public:
     Item() {}   // 2 defaults
-    Item(const char* n) :
-    name{n} {} // 1 member-list initializer, 1 default
-    Item(const char* n, int i) : name{n},
-    item{i} {} // 2 member-list initializers, 0 defaults
+    Item(const char* n) : name{n} {} // 1 member-list initializer, 1 default
+    Item(const char* n, int i) :
+    name{n}, item{i} {} // 2 member-list initializers, 0 defaults
+    
     const char* itemName() const { return name.c_str(); }
     int itemNo() const { return item; }
 };
 int main() {
-    std::cout << nItems << std::endl;
+    std::cout << nItems << std::endl; // 0
     Item a;
-    std::cout << a.itemName() << ' ' << a.itemNo() << '\n';
+    std::cout << a.itemName() << ' ' << a.itemNo() << '\n'; // empty 1
     Item b("apple");
-    std::cout << b.itemName() << ' ' << b.itemNo() << '\n';
+    std::cout << b.itemName() << ' ' << b.itemNo() << '\n'; // apple 2
     Item c("grape", 6);
-    std::cout << c.itemName() << ' ' << c.itemNo() << '\n';
-    std::cout << nItems << std::endl;
+    std::cout << c.itemName() << ' ' << c.itemNo() << '\n'; // grape 6
+    std::cout << nItems << std::endl; // 2
 }
