3a407856d9b68ccdd3ebe99da5f12861746ccc2e
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-06-08 12:27:57 -0400

    Update notes, W4

--- OOP345-Notes/w4_compositions_aggregations_associations/comp/Name.h
+++ OOP345-Notes/w4_compositions_aggregations_associations/comp/Name.h
@@ -2,13 +2,15 @@
 #define NAME_H
 // Composition - Name
 // Name.h
+// Pointer version requires Copy constructor, assignment operator and
+// destructor to deal with pointer!!!
 class Name {
     char* name { nullptr };
 public:
     Name(const char*);              // 1-Arg Constructor
-    Name(const Name&);              // Copy Constructor
-    Name& operator=(const Name&);   // Assignment Operator
-    ~Name();                        // Destructor
+    Name(const Name&);              // Copy Constructor, deep copying
+    Name& operator=(const Name&);   // Assignment Operator, deep assignment
+    ~Name();                        // Destructor, required
     const char* get() const;        // Getter
     void set(const char*);          // Setter
 };
